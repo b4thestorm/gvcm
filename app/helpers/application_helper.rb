1c2ef7396a2a5cb9a945b1d2bd87f90b619a0108
Arnold <arnoldsander@gmail.com>
2016-02-17 17:37:10 -0500

    spiffy homepage style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
 
   def embed(yt_id)
     youtube_id = yt_id
-    content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}", width: '600' , height: '400')
+    content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}", width: '600' , height: '400', autohide: '1', modestbranding: '1')
   end
 end
