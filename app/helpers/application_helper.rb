ac8108a2318e4e02ed4f58a87b19515b165c112a
Arnold <arnoldsander@gmail.com>
2016-02-18 09:51:20 -0500

    style fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
 
   def embed(yt_id)
     youtube_id = yt_id
-    content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}", width: '600' , height: '400', autohide: '1', modestbranding: '1')
+    content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}/sddefault.jpg", width: '600' , height: '400', autohide: '1', modestbranding: '1', rel: '0')
   end
 end
